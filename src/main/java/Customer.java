3611443e6b94272669f56a962e6bae9946b76f05
lkno0705 <lkno0705@gmail.com>
2021-05-11 14:33:00 +0200

    fixed unparameterized use of Enumeration in Customer.java

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -21,7 +21,7 @@ class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
+        Enumeration<Rental> enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
